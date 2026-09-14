2b5b6a6ce39d383b8863c4c600428ca1d423d708
Kęstutis Barzdaitis <kestutis.barzdaitis@icgfarma.eu>
2026-09-14 16:22:31 +0300

    Nauji pakeitimai

--- src/commands/commands.outlook.ts
+++ src/commands/commands.outlook.ts
@@ -1,33 +1,19 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
 /* global Office */
 
-/**
- * Show an outlook notification when the add-in command is executed.
- * @param event
- */
-export function setNotificationInOutlook(event: Office.AddinCommands.Event) {
-  const message: Office.NotificationMessageDetails = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
-
-  // Show a notification message.
-  Office.context.mailbox.item.notificationMessages.replaceAsync(
-    "ActionPerformanceNotification",
-    message
-  );
-
-  // Be sure to indicate when the add-in command function is complete.
-  event.completed();
+function onMessageComposeHandler(event: Office.AddinCommands.Event) {
+    const item = Office.context.mailbox.item;
+    if (item && item.itemType === Office.MailboxEnums.ItemType.Message) {
+        item.from.setAsync("warehouse@icgfarma.eu", (asyncResult) => {
+            if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                console.error("Nepavyko automatiškai pakeisti siuntėjo: " + asyncResult.error.message);
+            }
+            event.completed();
+        });
+    } else {
+        event.completed();
+    }
 }
 
-// Register the add-in commands with the Office host application.
-Office.onReady(async () => {
-  Office.actions.associate("action", setNotificationInOutlook);
+Office.onReady(() => {
+    Office.actions.associate("onMessageComposeHandler", onMessageComposeHandler);
 });
